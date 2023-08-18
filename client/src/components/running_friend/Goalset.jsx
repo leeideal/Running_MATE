f07d7865b4899293461901eeab8c9a66f7e88707
swimkim <81215099+swim-kim@users.noreply.github.com>
2023-08-18 17:29:55 +0900

    feat:dropdown수정

--- client/src/components/running_friend/Goalset.jsx
+++ client/src/components/running_friend/Goalset.jsx
@@ -10,9 +10,10 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const Container = styled.div`
     width: 341px;
-    height: 81px;
+    height: ${props => (props.open ? '305px':'81px')};
+
     flex-shrink: 0;
-    border-radius: ${props => (props.open ? '40px 40px 0 0' : '40px')};
+    border-radius: 40px;
     background: rgba(72, 93, 133, 0.70);
     backdrop-filter: blur(15px);
     display: flex; 
@@ -22,9 +23,20 @@ const Container = styled.div`
     top:99px;
     bottom:664px;
     transition: height 0.3s, border-radius 0.3s;
+
 `;
 
-const Gradiant = styled.div`
+const Downcontent = styled.div`
+    width:100%;
+    height:48%;
+    display: inline-flex;
+    padding-right: 0px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    position:absolute;
+    top:110px;
+    left:0px;
     background: linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(255, 255, 255, 0.41) 42.71%, rgba(255, 255, 255, 0.41) 51.56%, rgba(255, 255, 255, 0.33) 61.46%, rgba(255, 255, 255, 0.00) 100%);
 `;
 
@@ -33,6 +45,8 @@ const Flag = styled.img`
     height: 23px;
     flex-shrink: 0;
     margin-left: 31px;
+    position:fixed;
+    top:29px;
 `;
 
 const Text = styled.div`
@@ -44,6 +58,11 @@ const Text = styled.div`
     font-weight: 700;
     line-height: 32px; 
     margin-left: 11px;
+    position:fixed;
+    top:23px;
+    left:61px;
+
+
 `;
 
 const Dropdownbutton = styled.img`
@@ -52,61 +71,28 @@ const Dropdownbutton = styled.img`
     flex-shrink: 0;
     margin-left: 137px;
     cursor: pointer;
+    position:fixed;
+    top:34.39px;
+    right:29.25px;
 `;
 
-const DropdownContent = styled.div`
-    width: 341px;
-    height:224px;
-    flex-shrink: 0;
-    border-radius: 0 0 40px 40px;
-    background: rgba(72, 93, 133, 0.70);
-    backdrop-filter: blur(15px);
-    display: ${props => (props.open ? 'block' : 'none')};
-    position: absolute;
-    top: 100%;
-    left: 0;
-    padding: 16px;
-    flex-direction: row;
-    flex-wrap: wrap;
 
-`;
 
 const KmScroll = styled.div`
     max-height: 148px; /* Set a maximum height to make it scrollable */
     overflow-y: auto; /* Add this line to enable vertical scrolling */
-    position:relative;
-    left:-105px;
+    position:absolute;
+    left:15px;
+    top:0px;
 `;
 
-const CalorieScroll = styled.div`
-    max-height: 148px; /* Set a maximum height to make it scrollable */
-    overflow-y: auto; /* Add this line to enable vertical scrolling */
-    justify-content:center;
-    align-items:center;
-    position:relative;
-    bottom:145px;
-    
-`;
 
-const MinScroll = styled.div`
-    max-height: 148px; /* Set a maximum height to make it scrollable */
-    overflow-y: auto; /* Add this line to enable vertical scrolling */
-    justify-content:center;
-    align-items:center;
-    position:relative;
-    bottom:295px;
-    right:-105px;
-
-`;
 
 const DropdownColumn = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    
-
 `;
 
 const KmDropdownItem = styled.p`
@@ -114,13 +100,44 @@ const KmDropdownItem = styled.p`
     font-family: Outfit;
     color: ${props => (props.selected ? ' #AFFF7E' : '#B7C7DF')};
     font-size: ${props => (props.selected ? '40px ' : '28px')};
-    margin: 8px 0;
+    padding: 0px 0px 15px 15px;
     cursor: pointer;
     font-weight: 700;
     line-height: 40px; /* 100% */
-    
+   
+`;
+
+
+const Calorie =styled.div`
+    width: 90px;
+    color: var(--white, #AFFF7E);
+    text-align: center;
+    font-family: Outfit;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 40px; /* 100% */
+    position:absolute;
+    left:120px;
+    top:165px;
 
 `;
+
+const Min = styled.div`
+    width: 90px;
+    color: var(--white, #AFFF7E);
+    text-align: center;
+    font-family: Outfit;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 40px; /* 100% */
+    position:absolute;
+    right:25px;
+    top:165px;
+`;
+
+
 const TitleContainer = styled.div`
     display: inline-flex;
     width: 300px;
@@ -129,6 +146,9 @@ const TitleContainer = styled.div`
     gap: 15px;
     flex-shrink: 0;
     justify-content:center;
+
+    position:fixed;
+    top:75px;
 `;
 
 const MinTitle = styled.div`
@@ -187,8 +207,10 @@ const Distancegoal = styled.div`
   font-weight: 700;
   line-height: 40px; /* 80% */
   float:left;
-  position:absolute;
+  position:fixed;
   left:160px;
+  top:19px;
+
 
 `;
 
@@ -208,7 +230,10 @@ const Km = styled.div`
     top:39px;
 `;
 
-
+const DropdownContentContainer = styled.div`
+opacity: ${props => (props.animationStarted ? 1 : 0)};
+display: ${props => (props.animationStarted ? 'block' : 'none')};
+`;
 
 function Goalset() {
     const createOptions = (start, end, step) => {
@@ -218,122 +243,83 @@ function Goalset() {
         }
         return options;
       };
-    const createOptions2 = (start, end, step) => {
-        const options = [];
-        for (let value = start; value <= end; value += step) {
-          options.push(value.toFixed(0));
-        }
-        return options;
-      };
+
     const KmOptions = createOptions(0.2, 5.0, 0.1);
-    const CalorieOptions = createOptions2(20, 200, 1); 
-    const MinOptions = createOptions2(5, 30, 1);
-    
+
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [selectedKmOption, setSelectedKmOption] = useState(null);
-    const [selectedMinOption, setSelectedMinOption] = useState(null);
-    const [selectedCalorieOption, setSelectedCalorieOption] = useState(null);
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [calorieScrollTop, setCalorieScrollTop] = useState(0);
-    const [minScrollTop, setMinScrollTop] = useState(0);
+    const [selectedCalorie, setSelectedCalorie] = useState(null);
+    const [selectedMin, setSelectedMin] = useState(null);
+    const [animationStarted, setAnimationStarted] = useState(false);
+
     //const distancegoal = useSelector((state => state.distance));
     const dispatch = useDispatch();
     
     const handleDropdownClick = () => {
         setDropdownOpen(!dropdownOpen);
+        setAnimationStarted(true);
     };
 
-    const handleDropdownItemClick = (distance) => {
-        // Handle the selected distance (e.g., dispatch an action or update state)
-        console.log("Selected distance:", distance);
-        dispatch(setDistancegoal(distance));
-        setSelectedOption(distance);
+    // const handleDropdownItemClick = (distance) => {
+    //     // Handle the selected distance (e.g., dispatch an action or update state)
+    //     console.log("Selected distance:", distance);
+    //     dispatch(setDistancegoal(distance));
+    //     setSelectedOption(distance);
         
-    };
+    // };
     const handleKmDropdownItemClick = (km) => {
         setSelectedKmOption(km);
         const min = (km * 5).toFixed(0);
         const calorie = ((min / 5) * 49).toFixed(0);
-        setSelectedMinOption(min);
-        setSelectedCalorieOption(calorie);
+        setSelectedMin(min);
+        setSelectedCalorie(calorie);
         dispatch(setDistancegoal(km));
-        setCalorieScrollTop((calorie - 20) * 44); // Adjust the value as needed
-        setMinScrollTop((min - 5) * 44);
+
+
       };
     
-      const handleMinDropdownItemClick = (min) => {
-        setSelectedMinOption(min);
-        const km = (min / 5).toFixed(1);
-        const calorie = ((min / 5) * 49).toFixed(0);
-        setSelectedKmOption(km);
-        setSelectedCalorieOption(calorie);
-        dispatch(setDistancegoal(km));
-      };
-    
-      const handleCalorieDropdownItemClick = (calorie) => {
-        setSelectedCalorieOption(calorie);
-        const min = ((calorie / 49) * 5).toFixed(0);
-        const km = (min / 5).toFixed(1);
-        setSelectedMinOption(min);
-        setSelectedKmOption(km);
-        dispatch(setDistancegoal(km));
-        
-      };
+
 
   
-    return (
+      return (
         <Container open={dropdownOpen}>
             <Flag src={flagimg} />
             <Text>GOAL</Text>
             <Distancegoal>{selectedKmOption}</Distancegoal>
             <Km>KM</Km>
             <Dropdownbutton src={dropbutton} onClick={handleDropdownClick} />
-            <DropdownContent open={dropdownOpen}>
-                <DropdownColumn>
+
+            {/* Render the components conditionally based on dropdownOpen */}
+            {dropdownOpen && (
+                <DropdownContentContainer animationStarted={animationStarted}>
                     <TitleContainer>
                         <KmTtitle>KM</KmTtitle>
                         <CalorieTitle>CALORIE</CalorieTitle>
                         <MinTitle>MIN</MinTitle>
                     </TitleContainer>
-                    <KmScroll>
-                    {KmOptions.map((option, index) => (
-                        <KmDropdownItem
-                            key={index}
-                            selected={option === selectedKmOption}
-                            onClick={() => handleKmDropdownItemClick(option)}
-                        >
-                            {option}
-                        </KmDropdownItem>
-                    ))}
-                    </KmScroll>
-                    <CalorieScroll>
-                        {CalorieOptions.map((option, index) => (
-                            <KmDropdownItem  style={{ scrollTop: calorieScrollTop }}
-                                
-                                key={index}
-                                selected={option === selectedCalorieOption}
-                                onClick={() => handleCalorieDropdownItemClick(option)}    
-                            >
-                                {option}
-                            </KmDropdownItem>
-                        ))}
-                    </CalorieScroll>
-                    <MinScroll>
-                        {MinOptions.map((option, index) => (
-                                <KmDropdownItem
-                                    key={index}
-                                    selected={option === selectedMinOption}
-                                    onClick={() => handleMinDropdownItemClick(option)}   
-                                >
-                                    {option}
-                                </KmDropdownItem>
-                            ))}
-                    </MinScroll>
-                </DropdownColumn>
-            </DropdownContent>
+                    <Downcontent>
+                        <DropdownColumn>
+                            <KmScroll>
+                                {KmOptions.map((option, index) => (
+                                    <KmDropdownItem 
+                                        key={index}
+                                        selected={option === selectedKmOption}
+                                        onClick={() => handleKmDropdownItemClick(option)}
+                                    >
+                                        {option}
+                                    </KmDropdownItem>
+                                ))}
+                            </KmScroll>
+                        </DropdownColumn>
+                    </Downcontent>
+                    <Calorie>{selectedCalorie}</Calorie>
+                    <Min>{selectedMin}</Min>
+                </DropdownContentContainer>
+            )}
         </Container>
     );
 }
+
 
 export default Goalset;
